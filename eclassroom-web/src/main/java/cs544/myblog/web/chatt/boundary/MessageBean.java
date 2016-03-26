93ca9b52558e805b8d5f3315b52f902a590a14dd
bikesh_maharjan <bksmjn@gmail.com>
2016-03-26 15:10:46 -0500

    profile added

--- eclassroom-web/src/main/java/cs544/myblog/web/chatt/boundary/MessageBean.java
+++ eclassroom-web/src/main/java/cs544/myblog/web/chatt/boundary/MessageBean.java
@@ -9,79 +9,67 @@ package cs544.myblog.web.chatt.boundary;
  *
  * @author FaRiii
  */
-import cs544.myblog.web.chatt.entity.Message;
-import cs544.myblog.web.chatt.control.MessageManagerLocal;
-import java.io.Serializable;
-import java.util.Collections;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-import javax.ejb.EJB;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-import javax.faces.event.ActionEvent;
-import org.primefaces.context.RequestContext;
+
  
 /**
  *
  * @author Anton Danshin
  * Modified by FaRiii
  */
-@ManagedBean
-@ViewScoped
-public class MessageBean implements Serializable {
+
+public class MessageBean  {
  
-    @EJB
-    MessageManagerLocal mm;
- 
-    private final List messages;
-    private Date lastUpdate;
-    private Message message;
- 
-    /**
-     * Creates a new instance of MessageBean
-     */
-    public MessageBean() {
-        messages = Collections.synchronizedList(new LinkedList());
-        lastUpdate = new Date(0);
-        message = new Message();
-    }
- 
-    public Date getLastUpdate() {
-        return lastUpdate;
-    }
- 
-    public void setLastUpdate(Date lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
- 
-    public Message getMessage() {
-        return message;
-    }
- 
-    public void setMessage(Message message) {
-        this.message = message;
-    }
- 
-    public void sendMessage(ActionEvent evt) {
-        mm.sendMessage(message);
-    }
- 
-    public void firstUnreadMessage(ActionEvent evt) {
-       RequestContext ctx = RequestContext.getCurrentInstance();
- 
-       Message m = mm.getFirstAfter(lastUpdate);
- 
-       ctx.addCallbackParam("ok", m!=null);
-       if(m==null)
-           return;
- 
-       lastUpdate = m.getDateSent();
- 
-       ctx.addCallbackParam("user", m.getUser());
-       ctx.addCallbackParam("dateSent", m.getDateSent().toString());
-       ctx.addCallbackParam("text", m.getMessage());
- 
-    }
- 
+//    @EJB
+//    MessageManagerLocal mm;
+// 
+//    private final List messages;
+//    private Date lastUpdate;
+//    private Message message;
+// 
+//    /**
+//     * Creates a new instance of MessageBean
+//     */
+//    public MessageBean() {
+//        messages = Collections.synchronizedList(new LinkedList());
+//        lastUpdate = new Date(0);
+//        message = new Message();
+//    }
+// 
+//    public Date getLastUpdate() {
+//        return lastUpdate;
+//    }
+// 
+//    public void setLastUpdate(Date lastUpdate) {
+//        this.lastUpdate = lastUpdate;
+//    }
+// 
+//    public Message getMessage() {
+//        return message;
+//    }
+// 
+//    public void setMessage(Message message) {
+//        this.message = message;
+//    }
+// 
+//    public void sendMessage(ActionEvent evt) {
+//        mm.sendMessage(message);
+//    }
+// 
+//    public void firstUnreadMessage(ActionEvent evt) {
+//       RequestContext ctx = RequestContext.getCurrentInstance();
+// 
+//       Message m = mm.getFirstAfter(lastUpdate);
+// 
+//       ctx.addCallbackParam("ok", m!=null);
+//       if(m==null)
+//           return;
+// 
+//       lastUpdate = m.getDateSent();
+// 
+//       ctx.addCallbackParam("user", m.getUser());
+//       ctx.addCallbackParam("dateSent", m.getDateSent().toString());
+//       ctx.addCallbackParam("text", m.getMessage());
+// 
+//    }
+// 
 }
